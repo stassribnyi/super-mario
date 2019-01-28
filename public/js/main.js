7ce52a227abd1d92476cbe0ec4d70c6738c68009
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-01-28 08:44:40 +0200

    Split up functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,5 @@
-function loadImage(url) {
-  return new Promise(resolve => {
-    const image = new Image();
-
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-
-    image.src = url;
-  });
-}
-
-class SpriteSheet {
-  constructor(image, width, height) {
-    this.image = image;
-    this.width = width;
-    this.height = height;
-
-    this.tiles = new Map();
-  }
-
-  define(name, x, y) {
-    const buffer = document.createElement('canvas');
-
-    buffer.width = this.width;
-    buffer.height = this.height;
-
-    buffer
-      .getContext('2d')
-      .drawImage(
-        this.image,
-        x * this.width,
-        y * this.height,
-        this.width,
-        this.height,
-        0,
-        0,
-        this.width,
-        this.height
-      );
-
-    this.tiles.set(name, buffer);
-  }
-
-  draw(name, context, x, y) {
-    const buffer = this.tiles.get(name);
-
-    context.drawImage(buffer, x, y);
-  }
-}
+import SpriteSheet from './sprite-sheet.js';
+import { loadImage } from './loaders.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
