502449f34fb51210bc6de9b54524a9e443498af4
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-01-31 09:20:26 +0200

    Implement custom event listener handler

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,16 @@ import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 import { createMario } from './entities.js';
 import { loadLevel } from './loaders.js';
 
+import Keyboard from './keyboard-state.js';
 import Compositor from './compositor.js';
 import Timer from './timer.js';
+
+const input = new Keyboard();
+
+input.addMapping(32, keyState => {
+  console.log(keyState);
+});
+input.listenTo(window);
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -12,7 +20,7 @@ Promise.all([loadBackgroundSprites(), loadLevel('1-1'), createMario()]).then(
   ([backgroundSprites, level, mario]) => {
     const compositor = new Compositor();
 
-    const gravity = 30;
+    const gravity = 2000;
 
     mario.position.set(64, 180);
     mario.velocity.set(200, -600);
@@ -33,7 +41,7 @@ Promise.all([loadBackgroundSprites(), loadLevel('1-1'), createMario()]).then(
 
       mario.update(deltaTime);
 
-      mario.velocity.y += gravity;
+      mario.velocity.y += gravity * deltaTime;
     };
 
     timer.start();
